5118047285c97087edccf11f8d73052f0ec77539
Nicolas Goy <kuon@goyman.com>
2016-06-07 13:11:26 +0200

    Bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Backy.Mixfile do
 
   def project do
     [app: :backy,
-     version: "0.0.1",
+     version: "0.0.2",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
